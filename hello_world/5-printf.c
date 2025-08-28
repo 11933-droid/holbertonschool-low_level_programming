981f6400adbc1cd837bd71fbbc09d2af23785961
root <root@ip-10-42-49-109.ec2.internal>
2025-08-28 13:10:07 +0000

    Hello printf

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -3,10 +3,10 @@
 /**
  * main - this is printf function
  *
- * Return: Always 0 when successfull
+ * Return: Always 0 (Success)
  */
-int main (void)
+int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art\n,");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
