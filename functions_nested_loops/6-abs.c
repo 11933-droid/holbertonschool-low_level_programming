01962c7d82e4545a0ff58fb6250ec13496d0cdde
root <root@ip-10-42-153-129.ec2.internal>
2025-08-30 15:20:00 +0000

    Test 6

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _abs - computes the absolute value
+ * @n: the integer to check
+ *
+ * Return: absolute value of n
+ */
+
+int _abs(int n)
+{
+	if (n <0)
+		return (-n);
+	else
+		return (n);
+}
