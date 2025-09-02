94d1a70de87b0c3bdc3938d7d353547835f86c50
root <root@ip-10-42-213-232.ec2.internal>
2025-09-02 13:42:16 +0000

    Multiplication

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - multiplies tow integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: result of multiplication
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
