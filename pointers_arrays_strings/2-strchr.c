e8fa3f91f3700289f4165b5f3c14ca4f0232364c
11933-droid <alimalmaghrabi@gmail.com>
2025-09-12 10:32:47 +0000

    strchr

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strchr - locate character in string
+ * @s: string to search
+ * @c: character to search
+ *
+ * Return: s or NULL
+ */
+
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+			return (s);
+		s++;
+	}
+	if (*s == c)
+		return (s);
+	return (NULL);
+}
