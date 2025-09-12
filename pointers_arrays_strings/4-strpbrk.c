1a4f1486b9fb16de42ef6d61538fb8f522c5ad44
11933-droid <alimalmaghrabi@gmail.com>
2025-09-12 13:53:35 +0000

    strpbrk

---
+++ pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strpbrk - search for set byet
+ * @s: string to search
+ * @accept: string to search in s
+ *
+ * Return: accept or NULL
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	while (*s != '\0')
+	{
+		const char *a = accept;
+
+		while (*a != '\0')
+		{
+			if (*s == *a)
+				return (s);
+			a++;
+		}
+		s++;
+	}
+	return (NULL);
+}
