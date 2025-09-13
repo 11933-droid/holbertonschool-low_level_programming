61901319b31ae03a50dc1ed54d1d62cd308d951d
root <root@ip-10-42-169-18.ec2.internal>
2025-09-13 12:45:23 +0000

    Task 3

--- recursion/3-factorial.c
+++ recursion/3-factorial.c
@@ -6,13 +6,20 @@
  *
  * Return: factorial
  */
+
 int factorial(int n)
 {
 	if (n < 0)
-		 return (-1);
+	{	
+	       	return (-1);
+	}
 	else if (n == 0)
+	{
 		return (1);
+	}
 	else
+	{
 		return (n * factorial(n - 1));
+	}
 }
 
