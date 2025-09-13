a4c15431434920ec20a7021c5560e95e8769a201
root <root@ip-10-42-242-130.ec2.internal>
2025-09-13 01:31:19 +0000

    factorial

---
+++ recursion/3-factorial.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * factorial - find the factorial
+ * @n: number to find factorial
+ *
+ * Return: factorial
+ */
+int factorial(int n)
+{
+	if (n < 0)
+	       return (-1);
+	else if (n == 0)
+		return (1);
+	else
+		return (n * factorial(n - 1));
+}
+
