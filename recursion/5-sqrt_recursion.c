194adf8068f382d10df76e1702f60df176d9b5a8
root <root@ip-10-42-169-18.ec2.internal>
2025-09-13 13:49:58 +0000

    Task 5

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -1,6 +1,23 @@
 #include "main.h"
 
 /**
+ * _sqrt_check - helper function
+ * @n: number
+ * @g: current guess
+ *
+ * Return: square root
+ */
+
+int _sqrt_check(int n, int g)
+{
+	if (g * g == n)
+		return (g);
+	if (g * g > n)
+		return (-1);
+	return (_sqrt_check(n, g + 1));
+}
+
+/**
  * _sqrt_recursion - return natural square
  * @n: number to compute
  *
@@ -9,22 +26,9 @@
 
 int _sqrt_recursion(int n)
 {
-	static int g = 1;
-	int result;
-
 	if (n < 0)
 		return (-1);
-	if (g * g == n)
-	{
-		result = g;
-		g = 1;
-		return (result);
-	}
-	if (g * g > n)
-	{
-		g = 1;
-		return (-1);
-	}
-	g++;
-	return (_sqrt_recursion(n));
+	if (n == 0 || n == 1)
+		return (n);
+	return (_sqrt_check(n, 1));
 }
