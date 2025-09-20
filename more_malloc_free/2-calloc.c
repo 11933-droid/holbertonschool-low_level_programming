3ad03f382db0c24177d250cb485dcffaeb60dd58
root <root@ip-10-42-178-235.ec2.internal>
2025-09-20 14:40:06 +0000

    Task 2

---
+++ more_malloc_free/2-calloc.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * _calloc - allocate memory
+ * @nmemb: number of array
+ * @size: size of array
+ *
+ * Return: pointer to array
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *output;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	output = calloc(nmemb, size);
+	if (output == NULL)
+		return (NULL);
+	else
+		return (output);
+}
