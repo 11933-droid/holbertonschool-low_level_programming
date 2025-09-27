923ec85d98843bb64fb7f257d0cd376d2a99c256
root <root@ip-10-42-35-206.ec2.internal>
2025-09-27 14:23:19 +0000

    Last taskee

---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,66 @@
+#include "3-calc.h"
+
+/**
+ * op_add - adds two numbers
+ * @a: the first int
+ * @b: the second int
+ *
+ * Return: the sum
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - subtracts two numbers
+ * @a: the first int
+ * @b: the second int
+ *
+ * Return: the sub
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - multiplies two numbers
+ * @a: the first int
+ * @b: the second int
+ *
+ * Return: the mul
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - divides two numbers
+ * @a: the first int
+ * @b: the second int
+ *
+ * Return: the div
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - reminder of division
+ * @a: the first int
+ * @b: the second int
+ *
+ * Return: the mod
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
