e654292589cd91d9464cac167545a10e2297ba03
root <root@ip-10-42-250-210.ec2.internal>
2025-10-13 14:11:10 +0000

    Add node

---
+++ singly_linked_lists/2-add_node.c
@@ -0,0 +1,34 @@
+#include <string.h>
+#include "lists.h"
+
+/**
+ * add_node - adds a new node at the beginning of a list_t list
+ * @head: address of pointer to head node
+ * @str:  C-string to duplicate into the new node
+ *
+ * Return: address of the new element, or NULL on failure
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new_node;
+
+	if (head == NULL || str == NULL)
+		return (NULL);
+
+	new_node = malloc(sizeof(list_t));
+
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->str = strdup(str);
+	if (new_node->str == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+	new_node->len = (unsigned int)strlen(str);
+	new_node->next = *head;
+	*head = new_node;
+	return (new_node);
+}
