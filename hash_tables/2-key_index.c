74f03c5a76b91938ed97d302fb493366141afd27
root <root@ip-10-42-18-244.ec2.internal>
2025-10-24 12:56:49 +0000

    Key index

--- hash_tables/2-key_index.c
+++ hash_tables/2-key_index.c
@@ -5,7 +5,7 @@
  * @key: the key (string)
  * @size: size of array
  *
- * Return: the index where the key 
+ * Return: the index where the key
  */
 
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
