394b2438b426b7c18addf1e7dd1809787a559c9a
root <root@ip-10-42-18-244.ec2.internal>
2025-10-24 12:55:00 +0000

    Key index

---
+++ hash_tables/2-key_index.c
@@ -0,0 +1,14 @@
+#include "hash_tables.h"
+
+/**
+ * key_index - gives the index of a key
+ * @key: the key (string)
+ * @size: size of array
+ *
+ * Return: the index where the key 
+ */
+
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	return (hash_djb2(key) % size);
+}
